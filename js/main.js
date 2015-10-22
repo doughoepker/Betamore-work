0b0ca07bd9c22f424f5d769ec6531684e05b9e59
Tom Kraak <tom@backroom.io>
2015-10-21 21:56:21 -0400

    detect support for CSS blend modes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+// detecting support for CSS blend modes
 
+if(!('backgroundBlendMode' in document.body.style)) {
+    // No support for background-blend-mode
+  var html = document.getElementsByTagName("html")[0];
+  html.className = html.className + " no-background-blend-mode";
+}
